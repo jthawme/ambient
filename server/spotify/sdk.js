626b6cc90f9ede777c0351ca65ff25ae2a6f127c
Jonny Thaw <hi@jthaw.me>
2025-03-06 11:55:33 +0000

    fix: trying to fix issue with refresh token but i suspect its not done

--- server/spotify/sdk.js
+++ server/spotify/sdk.js
@@ -133,17 +133,8 @@ export async function persistSdk(
 	await fs.writeFile(p, JSON.stringify(accessTokenData), 'utf-8');
 
 	return new SpotifyApi(
-		new FixedAccessTokenStrategy(
-			client_id,
-			accessTokenData,
-			(_client_id, _client_secret, token) => {
-				SpotifyAuth.token.refresh(
-					client_id,
-					client_secret,
-					token.refresh_token,
-					token.access_token
-				);
-			}
+		new FixedAccessTokenStrategy(client_id, accessTokenData, (_client_id, _client_secret, token) =>
+			SpotifyAuth.token.refresh(client_id, client_secret, token.refresh_token, token.access_token)
 		),
 		{
 			deserializer: {
